8e29e7f563f12bf551a433f18e890013f050c4eb
tthugy <supermanace7@gmail.com>
2022-10-25 23:01:23 +0900

    update

--- src/App.svelte
+++ src/App.svelte
@@ -28,6 +28,12 @@
     padding: 1.5em;
     will-change: filter;
   }
+
+  .logo {
+    width: 2rem;
+    height: 1rem;
+    vertical-align: middle;
+  }
   .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
   }
